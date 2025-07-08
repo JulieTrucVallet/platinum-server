f402511c8b182d956f7f21b89cdbd043c675e507
Julie <julie.trucvallet@gmail.com>
2025-07-09 01:03:09 +0200

    update

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ app.use(cors(corsOptions));
 
 // Middlewares
 app.use(express.json());
-app.use("/uploads", (req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://platinum-client.onrender.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   next();
 });
